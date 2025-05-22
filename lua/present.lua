b3a449654a3adf948df5817640dbf321f29f0ed2
livingpool <52132459+livingpool@users.noreply.github.com>
2025-05-22 09:29:05 +0800

    tj p8: user customization

--- lua/present.lua
+++ lua/present.lua
@@ -1,7 +1,66 @@
 local M = {}
 
-M.setup = function()
-  --nothing
+--- Default executor for lua code
+--- @param block present.Block
+local execute_lua_code = function(block)
+  -- Override the default print function, to capture all of the output
+  -- Store the original print function
+  local original_print = print
+
+  -- Table to capture print messages
+  local output = {}
+
+  -- Redefine the print function
+  print = function(...)
+    local args = { ... }
+    local messages = table.concat(vim.tbl_map(tostring, args), "\t")
+    table.insert(output, messages)
+  end
+
+  -- Call the provided funtion
+  local chunk = loadstring(block.body)
+  pcall(function()
+    if not chunk then
+      table.insert(output, " <<< BROKEN CODE >>>")
+    else
+      chunk()
+    end
+
+    return output
+  end)
+
+  -- Restore the original print function
+  print = original_print
+
+  return output
+end
+
+M.create_system_executor = function(program)
+  return function(block)
+    local tempfile = vim.fn.tempname()
+    vim.fn.writefile(vim.split(block.body, "\n"), tempfile)
+    local result = vim.system({ program, tempfile }, { text = true }):wait()
+    return vim.split(result.stdout, "\n")
+  end
+end
+
+-- Default executors for languages
+local options = {
+  executors = {
+    lua = execute_lua_code,
+    javascript = M.create_system_executor("node"),
+    python = M.create_system_executor("python3"),
+  },
+}
+
+-- Setup the plugin
+M.setup = function(opts)
+  opts = opts or {}
+  opts.executors = opts.executors or {}
+
+  opts.executors.lua = opts.executors.lua or execute_lua_code
+  opts.executors.javascript = opts.executors.javascript or M.create_system_executor("node")
+  opts.executors.python = opts.executors.python or M.create_system_executor("python3")
 end
 
 --- @class present.Slides
@@ -199,12 +258,44 @@ M.start_presentation = function(opts)
     -- TODO: Make a way for people to execute this for other languages
     local block = slide.blocks[1]
     if not block then
-      vim.print("No blocks on this page")
+      print("No blocks on this page")
       return
     end
 
-    local chunk = loadstring(block.body)
-    chunk()
+    local executor = options.executors[block.language]
+    if not executor then
+      print("No valid executor for this language")
+      return
+    end
+
+    -- Table to capture print messages
+    local output = { "# Code", "", "```" .. block.language }
+    vim.list_extend(output, vim.split(block.body, "\n"))
+    table.insert(output, "```")
+
+    table.insert(output, "")
+    table.insert(output, "# Output")
+    table.insert(output, "")
+    table.insert(output, "```")
+    vim.list_extend(output, executor(block))
+    table.insert(output, "```")
+
+    local buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
+    local temp_width = math.floor(vim.o.columns * 0.8)
+    local temp_height = math.floor(vim.o.lines * 0.8)
+    vim.api.nvim_open_win(buf, true, {
+      relative = "editor",
+      style = "minimal",
+      noautocmd = true,
+      width = temp_width,
+      height = temp_height,
+      row = math.floor((vim.o.lines - temp_height) / 2),
+      col = math.floor((vim.o.columns - temp_width) / 2),
+      border = "rounded",
+    })
+
+    vim.bo[buf].filetype = "markdown"
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, output)
   end)
 
   local restore = {
@@ -260,7 +351,7 @@ end
 --   "# World",
 --   "this is another thing",
 -- }))
-M.start_presentation({ bufnr = 11 })
+-- M.start_presentation({ bufnr = 26 })
 
 M._parse_slides = parse_slides
 
